2dea690b833f930f54dd4cd25b3730e621404317
bryant <bryant@alphora.com>
2020-09-29 14:32:49 -0600

    changed to use oauth2 client to solve authentication issue

--- src/utils/medicationReferenceSolver.js
+++ src/utils/medicationReferenceSolver.js
@@ -1,17 +1,17 @@
+import FHIR from "fhirclient";
+
 export default function medicationReferenceSolver(referenceURL) {
     return Promise.all([getMedicationCode(referenceURL)])
 }
 
 async function getMedicationCode(orgReferenceURL) {
-
-    return fetch(orgReferenceURL, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
+        let request = orgReferenceURL;
+        if ((orgReferenceURL.indexOf('http') > -1) && (orgReferenceURL.indexOf('Medication/') > -1)) {
+            request = orgReferenceURL.substring(orgReferenceURL.indexOf('Medication/'));
         }
-    })
-    .then(function(response) {
-        console.log(response);
-        return response.code;
-    });
+       return FHIR.oauth2.ready()
+            .then(client => client.request(request))
+            .then(function (response) {
+                return response.code;
+            });
 }
