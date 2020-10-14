e0089f2e14e669f667194ff321bb6d483cb849f3
Patrick <hardig_pm@live.com>
2020-10-14 12:46:24 -0400

    Updated Header to have the notice to patients

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default class Header extends Component {
   static propTypes: any;
   render() {
-    const {
-      patientName, patientAge, patientGender, totalEntries, numFlaggedEntries, meetsInclusionCriteria
-    }: any = this.props;
+    // const {
+    //   patientName, patientAge, patientGender, totalEntries, numFlaggedEntries, meetsInclusionCriteria
+    // }: any = this.props;
 
     return (
       <header className="header">
@@ -21,8 +21,10 @@ export default class Header extends Component {
             alt="cds connect logo" /> */}
           <span className="header__logo-text">PainManager</span>
         </div>
-
-        <div className="header__summary">
+            <div className="header-notice">
+              <p>NOTE:  This summary is not intended for patients who are undergoing end-of-life care (hospice or palliative) or active cancer treatment.</p>
+            </div>
+        {/* <div className="header__summary">
           <div className="header__summary-patient">
             <FontAwesomeIcon className="patient-icon" icon="user-circle" title="patient" />
 
@@ -49,7 +51,7 @@ export default class Header extends Component {
               </div>
             </div>
           }
-        </div>
+        </div> */}
       </header>
     );
   }
