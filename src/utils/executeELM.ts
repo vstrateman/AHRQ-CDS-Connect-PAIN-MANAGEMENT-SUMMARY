7bf2d5cbcecd85f3dd03e1049bed4ec46cd7e516
bryant <bryant@alphora.com>
2020-10-23 11:31:43 -0600

    addition of getting questionnaire for cql usage in shared decision making

--- src/utils/executeELM.ts
+++ src/utils/executeELM.ts
@@ -17,217 +17,235 @@ import r4PainManagerELM from '../cql/r4/PainManager.json';
 // eslint-disable-next-line
 import valueSetDB from '../cql/valueset-db.json';
 import medicationReferenceSolver from "./medicationReferenceSolver";
+
 var Promise = require('es6-promise').Promise;
 var cqlResults = require('../helpers/cqlResults.json');
 
 let cqlFhirModule: any = cqlfhir;
+
 function executeELM(collector: any) {
-  let client: any, release: any, library: any;
-  return new Promise((resolve: any) => {
-    // First get our authorized client and send the FHIR release to the next step
-    const results = FHIR.oauth2.ready().then((clientArg) => {
-      client = clientArg;
-      return client.getFhirRelease();
-    })
-    // then remember the release for later and get the release-specific library
-    .then((releaseNum) => {
-      release = releaseNum;
-      library = getLibrary(release);
-    })
-    // then query the FHIR server for the patient, sending it to the next step
-    .then(() => {
-      return client.patient.read();
-    })
-    // then gather all the patient's relevant resource instances and send them in a bundle to the next step
-    .then((pt: any) => {
-      collector.push({ data: pt, url: 'Patient/' + pt.id});
-      let isFromOpiodRec = false;
-      const requests = extractResourcesFromELM(library,isFromOpiodRec).map((name) => {
-        if (name === 'Patient') {
-          return [pt];
-        }
-        return doSearch(client, release, name, collector);
-      });
-      // Don't return until all the requests have been resolved
-      return Promise.all(requests).then((requestResults: any) => {
-        const resources: any[] = [];
-        requestResults.forEach((result) => {resources.push(...result)});
-        return {
-          resourceType: "Bundle",
-          entry: resources.map(r => ({ resource: r }))
-        };
-      });
-    })
-    // then execute the library and return the results (wrapped in a Promise)
-    .then((bundle) => {
-      // const patientSource = getPatientSource(release);
-      // const codeService = new cql.CodeService(valueSetDB);
-      // const executor = new cql.Executor(library, codeService);
-      // patientSource.loadBundles([bundle]);
-      // const results = executor.exec(patientSource);
-      return cqlResults[Object.keys(cqlResults)[0]];
+    let client: any, release: any, library: any;
+    return new Promise((resolve: any) => {
+        // First get our authorized client and send the FHIR release to the next step
+        const results = FHIR.oauth2.ready().then((clientArg) => {
+            client = clientArg;
+            return client.getFhirRelease();
+        })
+            // then remember the release for later and get the release-specific library
+            .then((releaseNum) => {
+                release = releaseNum;
+                library = getLibrary(release);
+            })
+            // then query the FHIR server for the patient, sending it to the next step
+            .then(() => {
+                return client.patient.read();
+            })
+            // then gather all the patient's relevant resource instances and send them in a bundle to the next step
+            .then((pt: any) => {
+                collector.push({data: pt, url: 'Patient/' + pt.id});
+                let isFromOpiodRec = false;
+                const requests = extractResourcesFromELM(library, isFromOpiodRec).map((name) => {
+                    if (name === 'Patient') {
+                        return [pt];
+                    }
+                    return doSearch(client, release, name, collector);
+                });
+                // Don't return until all the requests have been resolved
+                return Promise.all(requests).then((requestResults: any) => {
+                    const resources: any[] = [];
+                    requestResults.forEach((result) => {
+                        resources.push(...result)
+                    });
+                    return {
+                        resourceType: "Bundle",
+                        entry: resources.map(r => ({resource: r}))
+                    };
+                });
+            })
+            // then execute the library and return the results (wrapped in a Promise)
+            .then((bundle) => {
+                const patientSource = getPatientSource(release);
+                const codeService = new cql.CodeService(valueSetDB);
+                const executor = new cql.Executor(library, codeService);
+                patientSource.loadBundles([bundle]);
+                const results = executor.exec(patientSource);
+                return results.patientResults[Object.keys(results.patientResults)[0]];
+//      return cqlResults[Object.keys(cqlResults)[0]];
+            });
+        resolve(results);
     });
-    resolve(results);
-  });
 }
 
 function getLibrary(release) {
-  switch(release) {
-    case 2:
-      return new cql.Library(dstu2FactorsELM, new cql.Repository({
-        CDS_Connect_Commons_for_FHIRv102: dstu2CommonsELM,
-        FHIRHelpers: dstu2HelpersELM
-      }));
-    case 4:
-      return new cql.Library(r4PainManagerELM, new cql.Repository({
-        CDS_Connect_Commons_for_FHIRv400: r4CommonsELM,
-        Factors: r4FactorsELM,
-        Concepts: r4ConceptsELM,
-        FHIRHelpers: r4HelpersELM,
-        ConversionFactors: r4ConversionFactorsELM,
-        MMECalculator: r4MMECalculatorELM,
-        OMTKLogic: r4OMTKLogicELM,
-        OMTKData: r4OMTKDataELM
-      }));
-    default:
-      throw new Error('Only FHIR DSTU2 and FHIR R4 servers are supported');
-  }
+    switch (release) {
+        case 2:
+            return new cql.Library(dstu2FactorsELM, new cql.Repository({
+                CDS_Connect_Commons_for_FHIRv102: dstu2CommonsELM,
+                FHIRHelpers: dstu2HelpersELM
+            }));
+        case 4:
+            return new cql.Library(r4PainManagerELM, new cql.Repository({
+                CDS_Connect_Commons_for_FHIRv400: r4CommonsELM,
+                Factors: r4FactorsELM,
+                Concepts: r4ConceptsELM,
+                FHIRHelpers: r4HelpersELM,
+                ConversionFactors: r4ConversionFactorsELM,
+                MMECalculator: r4MMECalculatorELM,
+                OMTKLogic: r4OMTKLogicELM,
+                OMTKData: r4OMTKDataELM
+            }));
+        default:
+            throw new Error('Only FHIR DSTU2 and FHIR R4 servers are supported');
+    }
 }
+
 // eslint-disable-next-line
 function getPatientSource(release) {
-  switch(release) {
-    case 2:
-      return cqlFhirModule.PatientSource.FHIRv102();
-      case 4:
-        return cqlFhirModule.PatientSource.FHIRv400();
-    default:
-      throw new Error('Only FHIR DSTU2 and FHIR R4 servers are supported');
-  }
+    switch (release) {
+        case 2:
+            return cqlFhirModule.PatientSource.FHIRv102();
+        case 4:
+            return cqlFhirModule.PatientSource.FHIRv400();
+        default:
+            throw new Error('Only FHIR DSTU2 and FHIR R4 servers are supported');
+    }
 }
 
 function doSearch(client, release, type, collector) {
-  const params = new URLSearchParams();
-  updateSearchParams(params, release, type);
+    const params = new URLSearchParams();
+    updateSearchParams(params, release, type);
 
-  const resources = [];
-  let uri = type + '?' + params;
-  if(type === 'MedicationRequest'){
-    uri = type + '?_include=MedicationRequest:medication';
-  }
-  if(type === 'MedicationStatement'){
-    uri = type + '?_include=MedicationStatement:medication';
-  }
-  return new Promise((resolve) => {
-    const results = client.patient.request(uri, {
-      pageLimit: 0, // unlimited pages
-      onPage: processPage(uri, collector, resources)
-    }).then(() => {
-      return resources;
-    }).catch((error) => {
-      collector.push({ error: error, url: uri, type: type, data: error });
-      // don't return the error as we want partial results if available
-      // (and we don't want to halt the Promis.all that wraps this)
-      return resources;
+    const resources: any[] = [];
+    let uri = type + '?' + params;
+    if (type === 'MedicationRequest') {
+        uri = type + '?_include=MedicationRequest:medication';
+    }
+    if (type === 'MedicationStatement') {
+        uri = type + '?_include=MedicationStatement:medication';
+    }
+    return new Promise((resolve) => {
+        if(type === 'Questionnaire') {
+            const qResults = client.request("Questionnaire/mypain-questionnaire")
+            .then((questionnaire) => {
+                if (questionnaire) {
+                    resources.push(questionnaire);
+                    collector.push({url: uri, data: questionnaire});
+                    return resources;
+                }
+            });
+            resolve(qResults);
+        }else{
+            const results = client.patient.request(uri, {
+                pageLimit: 0, // unlimited pages
+                onPage: processPage(uri, collector, resources)
+            }).then(() => {
+                return resources;
+            }).catch((error) => {
+                collector.push({error: error, url: uri, type: type, data: error});
+                // don't return the error as we want partial results if available
+                // (and we don't want to halt the Promis.all that wraps this)
+                return resources;
+            });
+            resolve(results);
+        }
     });
-    resolve(results);
-  });
 }
 
 function processPage(uri, collector, resources) {
-  return (bundle) => {
-    // Add to the collector
-    let url = uri;
-    if (bundle && bundle.link && bundle.link.some(l => l.relation === 'self' && l.url != null)) {
-      url = bundle.link.find(l => l.relation === 'self').url;
-    }
-    if(uri.startsWith('MedicationRequest') ||
-        uri.startsWith('MedicationStatement')){
-      bundle.entry.forEach((medReqEntry) =>{
-        if(((medReqEntry.resource.resourceType === 'MedicationRequest')
-            || (medReqEntry.resource.resourceType === 'MedicationStatement'))
-            && (medReqEntry.resource.medicationReference !== null)
-            && (medReqEntry.resource.medicationReference !== undefined)){
-          let reference = medReqEntry.resource.medicationReference.reference;
-          let referenceFound = false;
-          for(let medRefEntry of bundle.entry){
-            if (medRefEntry.resource.resourceType === 'Medication'
-                  && reference === 'Medication/' + medRefEntry.resource.id) {
-              medReqEntry.resource.medicationCodeableConcept = medRefEntry.resource.code;
-              referenceFound = true;
-            }
-          }
-          if(!referenceFound) {
-            medicationReferenceSolver(medReqEntry.resource.medicationReference.reference)
-                .then((code)=>{
-                  medReqEntry.resource.medicationReference = null;
-                  return medReqEntry.resource.medicationCodeableConcept = code[0];
-                });
-          }
+    return (bundle) => {
+        // Add to the collector
+        let url = uri;
+        if (bundle && bundle.link && bundle.link.some(l => l.relation === 'self' && l.url != null)) {
+            url = bundle.link.find(l => l.relation === 'self').url;
         }
-      });
+        if (uri.startsWith('MedicationRequest') ||
+            uri.startsWith('MedicationStatement')) {
+            bundle.entry.forEach((medReqEntry) => {
+                if (((medReqEntry.resource.resourceType === 'MedicationRequest')
+                    || (medReqEntry.resource.resourceType === 'MedicationStatement'))
+                    && (medReqEntry.resource.medicationReference !== null)
+                    && (medReqEntry.resource.medicationReference !== undefined)) {
+                    let reference = medReqEntry.resource.medicationReference.reference;
+                    let referenceFound = false;
+                    for (let medRefEntry of bundle.entry) {
+                        if (medRefEntry.resource.resourceType === 'Medication'
+                            && reference === 'Medication/' + medRefEntry.resource.id) {
+                            medReqEntry.resource.medicationCodeableConcept = medRefEntry.resource.code;
+                            referenceFound = true;
+                        }
+                    }
+                    if (!referenceFound) {
+                        medicationReferenceSolver(medReqEntry.resource.medicationReference.reference)
+                            .then((code) => {
+                                medReqEntry.resource.medicationReference = null;
+                                return medReqEntry.resource.medicationCodeableConcept = code[0];
+                            });
+                    }
+                }
+            });
+        }
+        collector.push({url: url, data: bundle});
+        // Add to the resources
+        if (bundle.entry) {
+            bundle.entry.forEach(e => resources.push(e.resource));
+        }
     }
-    collector.push({ url: url, data: bundle});
-    // Add to the resources
-    if (bundle.entry) {
-      bundle.entry.forEach(e => resources.push(e.resource));
-    }
-  }
 }
 
 function updateSearchParams(params, release, type) {
-  // If this is for Epic, there are some specific modifications needed for the queries to work properly
-  if (process.env.REACT_APP_EPIC_SUPPORTED_QUERIES
-    && process.env.REACT_APP_EPIC_SUPPORTED_QUERIES.toLowerCase() === 'true') {
-    if (release === 2) {
-      switch (type) {
-        case 'Observation':
-          // Epic requires you to specify a category or code search parameter, so search on all categories
-          params.set('category', [
-            'social-history', 'vital-signs', 'imaging', 'laboratory', 'procedure', 'survey', 'exam', 'therapy'
-          ].join(','));
-          break;
-        case 'MedicationOrder':
-          // Epic returns only active meds by default, so we need to specifically ask for other types
-          // NOTE: purposefully omitting entered-in-error
-          params.set('status', ['active', 'on-hold', 'completed', 'stopped', 'draft'].join(','));
-          break;
-        case 'MedicationStatement':
-          // Epic returns only active meds by default, so we need to specifically ask for other types
-          // NOTE: purposefully omitting entered-in-error
-          params.set('status', ['active', 'completed', 'intended'].join(','));
-          break;
-        default:
-          //nothing
-      }
-    } else if (release === 4) {
-      // NOTE: Epic doesn't currently support R4, but assuming R4 versions of Epic would need this
-      switch (type) {
-        case 'Observation':
-          // Epic requires you to specify a category or code search parameter, so search on all categories
-          params.set('category', [
-            'social-history', 'vital-signs', 'imaging', 'laboratory', 'procedure', 'survey', 'exam', 'therapy',
-            'activity'
-          ].join(','));
-          break;
-        case 'MedicationRequest':
-          // Epic returns only active meds by default, so we need to specifically ask for other types
-          // NOTE: purposefully omitting entered-in-error
-          params.set('status', [
-            'active', 'on-hold', 'cancelled', 'completed', 'stopped', 'draft', 'unknown'
-          ].join(','));
-          break;
-        case 'MedicationStatement':
-          // Epic returns only active meds by default, so we need to specifically ask for other types
-          // NOTE: purposefully omitting entered-in-error and not-taken
-          params.set('status', [
-            'active', 'completed', 'intended', 'stopped', 'on-hold', 'unknown'
-          ].join(','));
-          break;
-        default:
-          //nothing
-      }
+    // If this is for Epic, there are some specific modifications needed for the queries to work properly
+    if (process.env.REACT_APP_EPIC_SUPPORTED_QUERIES
+        && process.env.REACT_APP_EPIC_SUPPORTED_QUERIES.toLowerCase() === 'true') {
+        if (release === 2) {
+            switch (type) {
+                case 'Observation':
+                    // Epic requires you to specify a category or code search parameter, so search on all categories
+                    params.set('category', [
+                        'social-history', 'vital-signs', 'imaging', 'laboratory', 'procedure', 'survey', 'exam', 'therapy'
+                    ].join(','));
+                    break;
+                case 'MedicationOrder':
+                    // Epic returns only active meds by default, so we need to specifically ask for other types
+                    // NOTE: purposefully omitting entered-in-error
+                    params.set('status', ['active', 'on-hold', 'completed', 'stopped', 'draft'].join(','));
+                    break;
+                case 'MedicationStatement':
+                    // Epic returns only active meds by default, so we need to specifically ask for other types
+                    // NOTE: purposefully omitting entered-in-error
+                    params.set('status', ['active', 'completed', 'intended'].join(','));
+                    break;
+                default:
+                //nothing
+            }
+        } else if (release === 4) {
+            // NOTE: Epic doesn't currently support R4, but assuming R4 versions of Epic would need this
+            switch (type) {
+                case 'Observation':
+                    // Epic requires you to specify a category or code search parameter, so search on all categories
+                    params.set('category', [
+                        'social-history', 'vital-signs', 'imaging', 'laboratory', 'procedure', 'survey', 'exam', 'therapy',
+                        'activity'
+                    ].join(','));
+                    break;
+                case 'MedicationRequest':
+                    // Epic returns only active meds by default, so we need to specifically ask for other types
+                    // NOTE: purposefully omitting entered-in-error
+                    params.set('status', [
+                        'active', 'on-hold', 'cancelled', 'completed', 'stopped', 'draft', 'unknown'
+                    ].join(','));
+                    break;
+                case 'MedicationStatement':
+                    // Epic returns only active meds by default, so we need to specifically ask for other types
+                    // NOTE: purposefully omitting entered-in-error and not-taken
+                    params.set('status', [
+                        'active', 'completed', 'intended', 'stopped', 'on-hold', 'unknown'
+                    ].join(','));
+                    break;
+                default:
+                //nothing
+            }
+        }
     }
-  }
 }
 
 export default executeELM;
