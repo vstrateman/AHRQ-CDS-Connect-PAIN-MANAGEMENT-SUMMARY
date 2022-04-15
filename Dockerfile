7fa0206d6375da723c26433fbabf9b11e165a90a
capt-mac <jrmckelvy@gmail.com>
2022-04-15 07:35:40 -0600

    change env for ruler, change path, change node version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:14 as builder
+FROM node:14.0.0 as builder
 
 RUN mkdir -p /home/node/app && chown -R node:node /home/node/app
 WORKDIR /home/node/app
--- src/containers/Root.js
+++ src/containers/Root.js
@@ -4,11 +4,17 @@ import { Route, Switch } from 'react-router-dom';
 import App from './App';
 import Landing from '../components/Landing';
 
+let pathPrefix = '';
+if (process.env.REACT_APP_PUBLIC_URL) {
+    const publicUrl = new URL(process.env.REACT_APP_PUBLIC_URL);
+    pathPrefix = publicUrl.pathname;
+}
+
 const Root = (props) => {
   return (
     <App>
       <Switch>
-        <Route exact path="/" component={Landing} />
+        <Route exact path={pathPrefix + '/'} component={Landing} />
       </Switch>
     </App>
   );
